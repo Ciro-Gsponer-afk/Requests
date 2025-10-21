3b4febbe6363b902446540c35dbe88cdb13557d4
Ciro Gsponer <cirogsp235@gmail.com>
2025-10-21 10:54:45 -0300

    Segundo Commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,20 @@
 import { useState } from 'react'
 
 import './App.css'
-
 function App() {
   const [image, setImage] = useState()
 
   const Obtenerimagen = () => {
     fetch("https://dog.ceo/api/breeds/list/all")
     .then(data => data.json())
-    .then(Obj => setImage(Obj.message) )
+    .then(Obj => setImage(Obj.message))
 }
+
 
   return (
     <>
+      <h1> MOSTRAR FOTOS DE PERROS  </h1>
+          <button onClick={Obtenerimagen} > Mostrar Fotos </button>
     </>
   )
 }
