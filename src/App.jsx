f23a06219c47675da2c782859c2ddeb83b55a2f0
Ciro Gsponer <cirogsp235@gmail.com>
2025-10-24 11:14:18 -0300

    Sin Rutas

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,40 @@ import { useState } from 'react'
 
 import './App.css'
 function App() {
-  const [image, setImage] = useState()
+  const [name, Setname] = useState("")
+  const [Pais, SetPais] = useState("")
+  const [Datos, SetDatos] = useState("")
+  const ObtenerPais = () => {
+    fetch(`https://restcountries.com/v3.1/name/${name}`)
+      .then(data => data.json())
+      .then(Arr => SetPais(Arr[0]))
+  }
 
-  const Obtenerimagen = () => {
-    fetch("https://dog.ceo/api/breeds/list/all")
-    .then(data => data.json())
-    .then(Obj => setImage(Obj.message))
-}
+  { console.log(name, Pais); }
+
+
 
 
   return (
     <>
-      <h1> MOSTRAR FOTOS DE PERROS  </h1>
-          <button onClick={Obtenerimagen} > Mostrar Fotos </button>
+      <h1> INFO PAISES  </h1>
+      <input type='text' onChange={(e) => Setname(e.target.value)} ></input>
+      <button onClick={ObtenerPais} >Mostrar Pais</button>
+      {Pais && (
+        <div>
+          <h2>{Pais.name.common}</h2>
+          <p><strong>Nombre oficial:</strong> {Pais.name.official}</p>
+          <img src={Pais.flags.png} alt={`Bandera de ${Pais.name.common}`} width="150" />
+          <p><strong>Capital:</strong> {Pais.capital?.[0]}</p>
+          <p><strong>Región:</strong> {Pais.region}</p>
+          <p><strong>Población:</strong> {Pais.population.toLocaleString()}</p>
+          <p><strong>Área:</strong> {Pais.area.toLocaleString()} km²</p>
+          <p><strong>Moneda:</strong> {Object.values(Pais.currencies || {})[0]?.name}</p>
+          <p><strong>Idioma principal:</strong> {Object.values(Pais.languages || {})[0]}</p>
+        
+        </div>)}
+
     </>
   )
 }
-
 export default App
